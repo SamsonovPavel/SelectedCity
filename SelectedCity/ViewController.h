e68b35f9bce17e9ff2e9d758cf46c29c927b6a9d
Samsonov Pavel <samsonovpavel@mail.ru>
2016-11-25 23:16:16 +0300

    Initial project

---
+++ SelectedCity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelectedCity
+//
+//  Created by Pavel Samsonov on 25.11.16.
+//  Copyright © 2016 Pavel Samsonov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
